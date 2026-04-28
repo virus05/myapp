cf8592bb843adfd43f68df0b612741fd3f84a6be
Eugen Chirilov <e_chirilov@yahoo.com>
2026-04-28 10:49:52 +0100

    change app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "Hello Eugeniu, This is your first app deployed in container"
+	return "!!! Hello Eugeniu, This is your first app deployed in container !!!"
 
 app.run(host="0.0.0.0", port=5000)
